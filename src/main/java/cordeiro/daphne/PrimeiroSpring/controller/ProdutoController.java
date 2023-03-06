e0b8ea7f3d2e784b3a88b39289f6e2e8ffec1b92
Daphne Cordeiro <daphnecordeiro22@gmail.com>
2023-03-06 18:13:13 -0300

    controller fechado

--- src/main/java/cordeiro/daphne/PrimeiroSpring/controller/ProdutoController.java
+++ src/main/java/cordeiro/daphne/PrimeiroSpring/controller/ProdutoController.java
@@ -5,7 +5,12 @@ import cordeiro.daphne.PrimeiroSpring.dto.ProdutoResponse;
 import cordeiro.daphne.PrimeiroSpring.model.Produto;
 import cordeiro.daphne.PrimeiroSpring.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("produto")
@@ -20,29 +25,44 @@ public class ProdutoController {
         produto.setMarca(produtoRequest.getMarca());
         produto.setPreco(produtoRequest.getPreco());
         produto.setQuantidade(produtoRequest.getQuantidade());
-        produto= produtoService.criar(produto);
+        produto = produtoService.criar(produto);
         return produto.getId();
     }
 
+    @GetMapping
+    public List<ProdutoResponse> readAll() {
+        return produtoService.obterTodos().stream().map(produto -> {
+            ProdutoResponse produtoResponse = new ProdutoResponse();
+            BeanUtils.copyProperties(produto, produtoService);
+            return produtoResponse;
+        }).collect(Collectors.toList());
+    }
     @GetMapping("{id}")
     public ProdutoResponse read(@PathVariable String id) {
-       Produto produto = produtoService.selecionarProduto(id);
-       ProdutoResponse produtoResponse = new ProdutoResponse();
+        Produto produto = produtoService.selecionarProduto(id);
+        ProdutoResponse produtoResponse = new ProdutoResponse();
         produtoResponse.setNome(produto.getNome());
         produtoResponse.setMarca(produto.getMarca());
         produtoResponse.setPreco(produto.getPreco());
         produtoResponse.setQuantidade(produto.getQuantidade());
-        return  produtoResponse;
+        return produtoResponse;
     }
 
     @PutMapping
-    public ProdutoResponse update(String id, ProdutoRequest produtoRequest) {
-        return new ProdutoResponse();
+    public ProdutoResponse update(@PathVariable String id, @RequestBody ProdutoRequest produtoRequest) {
+        Produto produto = produtoService.obter(id);
+        BeanUtils.copyProperties(produtoRequest, produto);
+        produto = produtoService.atualizar(produto);
+        ProdutoResponse produtoResponse = new ProdutoResponse();
+        BeanUtils.copyProperties(produto, produtoResponse);
+        return produtoResponse;
     }
 
     @DeleteMapping
-    public void delete(String id) {
+    public void delete(@PathVariable String id) {
+        produtoService.deletarProduto(id);
     }
 
-
 }
+
+
--- src/main/java/cordeiro/daphne/PrimeiroSpring/service/ProdutoService.java
+++ src/main/java/cordeiro/daphne/PrimeiroSpring/service/ProdutoService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -15,8 +17,8 @@ public class ProdutoService {
 
     private final ProdutoRepository produtoRepository;
 
-    public Produto criar(Produto produto){
-        if (produto.getId()!=null){
+    public Produto criar(Produto produto) {
+        if (produto.getId() != null) {
             throw new IllegalArgumentException();
         }
         produto.setId(UUID.randomUUID().toString());
@@ -24,9 +26,29 @@ public class ProdutoService {
         return produtoRepository.save(produto);
     }
 
-    public Produto selecionarProduto(String id){
-      return  produtoRepository.findById(id).orElse(null);
+    public Produto selecionarProduto(String id) {
+        return produtoRepository.findById(id).orElse(null);
     }
 
+    public Produto deletarProduto(String id) { // depois da aula
+        Produto produto = produtoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException());
+        produtoRepository.delete(produto);
+        return produto;
+    }
 
-}
+    public Produto obter(String id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    public List<Produto> obterTodos() {
+        List<Produto> lista = new ArrayList<>();
+        produtoRepository.findAll().forEach(comodo -> lista.add(comodo));
+        return lista;
+    }
+
+    public Produto atualizar(Produto produto){
+            produto.setUltimoPedido(LocalDateTime.now());
+            return produtoRepository.save(produto);
+        }
+    }
+
